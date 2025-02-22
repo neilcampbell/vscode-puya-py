489e8404c223ecac1ce38da855bb0866c40a39eb
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-02-22 11:26:36 +1000

    chore: start npx from shell

--- src/features/typescript/language-server.ts
+++ src/features/typescript/language-server.ts
@@ -1,50 +1,21 @@
-import { ChildProcess, spawn } from 'child_process'
 import { window } from 'vscode'
 import { WorkspaceFolder } from 'vscode'
-import { LanguageClient, LanguageClientOptions, MessageTransports, ServerOptions, TransportKind } from 'vscode-languageclient/node'
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node'
 
 const clients: Map<string, LanguageClient> = new Map()
-
-async function tryToRunCommand(command: string): Promise<boolean> {
-  try {
-    const serverProcess = spawn('C:\\Program Files\\nodejs\\npx.cmd', ['-v'])
-    if (!serverProcess || !serverProcess.pid) {
-      handleChildProcessStartError(serverProcess, `Launching server using command ${command} failed.`)
-      return false
-    }
-    return true
-  } catch {
-    return false
-  }
-}
-
-function handleChildProcessStartError(process: ChildProcess, message: string) {
-  if (process === null) {
-    return Promise.reject<MessageTransports>(message)
-  }
-
-  return new Promise<MessageTransports>((_, reject) => {
-    process.on('error', (err) => {
-      reject(`${message} ${err}`)
-    })
-    // the error event should always be run immediately,
-    // but race on it just in case
-    setImmediate(() => reject(message))
-  })
-}
 
 export async function startLanguageServer(workspaceFolder: WorkspaceFolder) {
   if (clients.has(workspaceFolder.name)) {
     return
   }
 
-  tryToRunCommand('node -v')
   const serverOptions: ServerOptions = {
-    command: 'C:\\Program Files\\nodejs\\npx.cmd',
+    command: 'npx',
     args: ['run-language-server'],
     transport: TransportKind.stdio,
     options: {
       cwd: workspaceFolder.uri.fsPath,
+      shell: true,
     },
   }
 
